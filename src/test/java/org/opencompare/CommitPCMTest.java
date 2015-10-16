1fe090d9189b0d5a40de930c36c9cacafd170d89
NicolasSellier <sellier.nicolas@etudiant.univ-rennes1.fr>
2015-10-16 14:44:44 +0200

    A new test case

---
+++ src/test/java/org/opencompare/CommitPCMTest.java
@@ -0,0 +1,14 @@
+package org.opencompare;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommitPCMTest {
+
+	
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+}
