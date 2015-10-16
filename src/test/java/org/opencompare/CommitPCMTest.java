28981b96b1534c6a829db5103809693e5642dc5a
NicolasSellier <sellier.nicolas@etudiant.univ-rennes1.fr>
2015-10-16 15:11:46 +0200

    fix

---
+++ src/test/java/org/opencompare/CommitPCMTest.java
@@ -0,0 +1,16 @@
+package org.opencompare;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommitPCMTest {
+
+	
+	@Test
+	public void test1() {
+		// simple verification
+		assertTrue(true); // everything OK
+	}//fin
+}
+//ufuygkuygferf
